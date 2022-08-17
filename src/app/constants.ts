fc86e0c562710279bd89ec7a8b8f8968b1c6b499
Andrew Byrne <andrew.byrne@blueface.com>
2022-08-17 09:02:58 +0100

    diminished chords

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,9 +2,11 @@ export const Constants = {
   ALL_NOTES: ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
   MAJOR_SCALE: [0, 2, 4, 5, 7, 9, 11],
   MINOR_SCALE: [0, 2, 3, 5, 7, 8, 10],
+  DIMINISHED_SCALE: [0, 1, 3, 5, 6, 8, 10],
   STANDARD_TUNING: ['E', 'A', 'D', 'G', 'B', 'E'],
 
   MAJOR_INTERVALS: ['major', 'minor', 'minor', 'major', 'major', 'minor', 'dim'],
-  MINOR_INTERVALS: ['minor', 'dim', 'major', 'minor', 'minor', 'major', 'major']
+  MINOR_INTERVALS: ['minor', 'dim', 'major', 'minor', 'minor', 'major', 'major'],
+
 
 }
--- src/app/tuning.service.ts
+++ src/app/tuning.service.ts
@@ -26,28 +26,36 @@ export class ChordinateService {
     this.scaleSubject.next(key);
   }
 
-  getNotesInScale(key, scale) {
+  getNotesInScale(key: string, scale: number[]) {
     const notesOrderedFromRoot = this.orderNotesFromRoot(key)
     return scale.map(interval => notesOrderedFromRoot[interval])
   }
 
-  orderNotesFromRoot(key) {
+  orderNotesFromRoot(key: string) {
     const keyIndex = Constants.ALL_NOTES.indexOf(key)
     return [...Constants.ALL_NOTES.slice(keyIndex, Constants.ALL_NOTES.length), ...Constants.ALL_NOTES.slice(0, keyIndex)]
   }
 
   getChords(key: IScale){
-    const voice = key.voice === 'major' ? Constants.MAJOR_SCALE : Constants.MINOR_SCALE;
-    const intervals = key.voice === 'major' ? Constants.MAJOR_INTERVALS : Constants.MINOR_INTERVALS;
+    const voice = this._getVoice(key.voice);
+    const intervals = this._getVoice(key.voice);
     const notesInScale = this.getNotesInScale(key.key, voice);
     return notesInScale.map((note, i) => ({root: note, voice: intervals[i]}));
   }
 
   setChord(chord: IChord){
-    const voice = chord.voice === 'major' ? Constants.MAJOR_SCALE : Constants.MINOR_SCALE;
+    const voice = this._getVoice(chord.voice);
     const notesInScale = this.getNotesInScale(chord.root, voice);
     chord.triad = [notesInScale[0], notesInScale[2], notesInScale[4]];
     this.chordSubject.next(chord);
   }
+
+	_getVoice(voice: string){
+		switch (voice) {
+			case 'major' : return Constants.MAJOR_SCALE;
+			case 'minor' : return Constants.MINOR_SCALE;
+			case 'diminished' : return Constants.DIMINISHED_SCALE;
+		}
+	}
 
 }
